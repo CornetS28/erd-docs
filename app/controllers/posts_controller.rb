b1d8328c51e8b16f00d627cefdd823f615a671bf
Urchmaney <kingsobino@gmail.com>
2019-09-13 23:45:24 +0000

    Resolve stickler error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,6 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @posts = current_user.posts
-    
     @posts = @posts.sort { |a, b| b.created_at - a.created_at } unless @posts.empty?
   end
 
@@ -28,9 +27,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-   
-  end
+  def show;end
 
   def edit; end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :posts, foreign_key: 'author_id', dependent: :destroy
-    
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
-    validates :email, format: { with: VALID_EMAIL_REGEX }
-    validates :surname,:email,:first_name, presence: true
-
-  end
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :surname, :email, :first_name, presence: true
+end
   
