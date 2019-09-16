5442b0a9690f87220acf6a1ccf9eeb23eca433cf
Urchmaney <kingsobino@gmail.com>
2019-09-16 21:40:18 +0000

    stickler errors resolve

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@
 
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-	def create
-		@comment = Comment.new(user_id: current_user.id,post_id: params[:post_id],comment: params[:comment])
-		@comment.save
-		redirect_back(fallback_location: root_path)
+  def create
+    @comment = Comment.new(user_id: current_user.id, post_id: params[:post_id], comment: params[:comment])
+    @comment.save
+    redirect_back(fallback_location: root_path)
 	end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -24,4 +24,3 @@ class LikesController < ApplicationController
     end
   end
 end
-  
