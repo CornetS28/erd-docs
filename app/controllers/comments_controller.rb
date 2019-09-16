305c1fab985f2801cbc38835ad232c8cff3ba97a
Urchmaney <kingsobino@gmail.com>
2019-09-16 21:04:54 +0000

    Added the comments functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+	def create
+		@comment = Comment.new(user_id: current_user.id,post_id: params[:post_id],comment: params[:comment])
+		@comment.save
+		redirect_back(fallback_location: root_path)
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_173018) do
+ActiveRecord::Schema.define(version: 2019_09_16_191714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
