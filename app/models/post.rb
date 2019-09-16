7d8d371fa8dbbcb290ed8fb15f82f7e08dc72a1d
SamuelC28 <samuel@noukod.com>
2019-09-16 14:17:43 -0400

    likes feature added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :likes, dependent: :destroy
   validates :content, length: { maximum: 285 }, presence: true
 end
