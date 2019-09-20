fddaa436e4d0dbe20ca3784dab4a2b21577aefa2
Urchmaney <kingsobino@gmail.com>
2019-09-20 21:04:23 +0000

    Resolve the test error

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@ class User < ApplicationRecord
   has_many :friend_requests, through: :user_friend_requests, source: :friend
   has_many :request_users, -> { where(status: false) }, foreign_key: :friend_id, class_name: :Friend
   has_many :requests, through: :request_users, source: :user
-
-  has_many :notifications, foreign_key: :recipient_id
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :surname, :email, :first_name, presence: true
